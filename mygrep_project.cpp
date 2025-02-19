6f21cccce8c84ea5e547445169c984aea9c2b83f
Kameji <diman567tak@gmail.com>
2025-02-19 12:33:45 +0200

    Implemented program version for 4 points, added settings and two parameters for them as well as error handling

--- mygrep_project.cpp
+++ mygrep_project.cpp
@@ -6,6 +6,93 @@
 
 using namespace std;
 
+enum SettingsOptions
+{
+    DisplayLineNumbers,
+    DisplayOccurenceAmount,
+    EnableReverceSearch,
+    DisableCaseSensitivity
+};
+
+struct mathingString
+{
+    int index;
+    string line;
+};
+
+struct settings
+{
+    private: 
+        vector<SettingsOptions> options;
+        vector<char> optionsChars;
+
+    public :
+        void addOption(char option)
+    {
+        if(find(optionsChars.begin(), optionsChars.end(), option) != optionsChars.end())
+		{
+            throw exception("You cannot set the same settings paramether twice!");
+        }
+
+        switch (option)
+        {
+            case 'l':
+				options.push_back(DisplayLineNumbers);
+				break;
+			case 'o':
+				options.push_back(DisplayOccurenceAmount);
+				break;
+			case 'r':
+				options.push_back(EnableReverceSearch);
+				break;
+			case 'i':
+				options.push_back(DisableCaseSensitivity);
+				break;
+			default:
+				throw exception("Unknown settings paramether");
+        }
+
+        optionsChars.push_back(option);
+    }
+
+        bool isDisplayLineNumbersEnabled()
+	{
+		return find(options.begin(), options.end(), DisplayLineNumbers) != options.end();
+	}
+    
+        bool isDisplayOccurenceAmountEnabled()
+	{
+		return find(options.begin(), options.end(), DisplayOccurenceAmount) != options.end();
+	}
+    
+        bool isReverceSearchEnabled()
+	{
+		return find(options.begin(), options.end(), EnableReverceSearch) != options.end();
+	}
+    
+        bool isCaseSensitivityEnabled()
+	{
+		return find(options.begin(), options.end(), DisableCaseSensitivity) == options.end();
+	}
+};
+
+void processSettings(string settingsString, settings* programSettings)
+{
+    if(settingsString.substr(0, 2) == "-o")
+	{
+		settingsString = settingsString.substr(2);
+    }
+    else
+    {
+        throw exception("Settings paramether should start with \"-o\"");
+    }
+
+    while (settingsString.length() > 0)
+    {
+        programSettings->addOption(settingsString[0]);
+        settingsString = settingsString.substr(1);
+    }
+}
 //0 - no string appearence
 //1 or above - string index
 int searchAString(string searchFrom, string whatToSearch)
@@ -17,9 +104,9 @@ int searchAString(string searchFrom, string whatToSearch)
     return result == string::npos ? 0 : result;
 }
 
-vector<string> searchAFile(ifstream& file, string whatToSearch)
+vector<mathingString> searchAFile(ifstream& file, string whatToSearch)
 {
-    vector<string> result;
+    vector<mathingString> result;
     string lineToSearch;
 
     while (!file.eof())
@@ -29,7 +116,8 @@ vector<string> searchAFile(ifstream& file, string whatToSearch)
 
         if (lineSearchResult > 0)
         {
-            result.push_back(lineToSearch);
+            mathingString res = {lineSearchResult, lineToSearch};
+            result.push_back(res);
         }
     }
 
@@ -38,7 +126,7 @@ vector<string> searchAFile(ifstream& file, string whatToSearch)
 
 int main(int argc, char *argv[])
 {
-    vector<string> result;
+    vector<mathingString> result;
     string whatToSearch;
 
     try
@@ -77,14 +165,37 @@ int main(int argc, char *argv[])
             return 0;
         }
 
-        ifstream textFileToSearch(argv[2], std::ifstream::in);
+        //We always want to use last arguments as file name
+        ifstream textFileToSearch(argv[argc - 1], std::ifstream::in);
+        settings programSettings = {};
 
-        result = searchAFile(textFileToSearch, argv[1]);
+        if (!textFileToSearch.is_open())
+            throw exception("File not found");
+
+        if(argc > 3)
+            processSettings(argv[argc - 3], &programSettings);
+
+        //We always want to use last but one argument as string to search
+        result = searchAFile(textFileToSearch, argv[argc - 2]);
+        textFileToSearch.close();
 
         for (uint_fast8_t i = 0; i < result.size(); i++)
         {
-            cout << result[i] << " \n";
+            if(programSettings.isDisplayLineNumbersEnabled())
+				cout << result[i].index << ":     ";
+            cout << result[i].line << " \n";
+
         }
+        
+        if(programSettings.isDisplayOccurenceAmountEnabled())
+            cout << "Occurrences of lines containing "<< whatToSearch << ": " << result.size() << "\n";
+
+        if(result.size() == 0)
+		{
+			cout << "No mathing strings were found in the file";
+		}
+
+        return 0;
     } 	catch (exception e)
 	{
 		cout << "Error: " << e.what();
