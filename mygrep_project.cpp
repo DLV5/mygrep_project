1a69e79857d7177dcc931150c6915aec0d601349
Kameji <diman567tak@gmail.com>
2025-02-19 10:55:58 +0200

    Added reading for the file using console, updated table

--- mygrep_project.cpp
+++ mygrep_project.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
+#include <filesystem>
 
 using namespace std;
 
@@ -14,34 +17,77 @@ int searchAString(string searchFrom, string whatToSearch)
     return result == string::npos ? 0 : result;
 }
 
-int main()
+vector<string> searchAFile(ifstream& file, string whatToSearch)
 {
-    size_t result;
-    string searchFrom;
+    vector<string> result;
+    string lineToSearch;
+
+    while (!file.eof())
+    {
+        getline(file, lineToSearch);
+        int lineSearchResult = searchAString(lineToSearch, whatToSearch);
+
+        if (lineSearchResult > 0)
+        {
+            result.push_back(lineToSearch);
+        }
+    }
+
+    return result;
+}
+
+int main(int argc, char *argv[])
+{
+    vector<string> result;
     string whatToSearch;
 
-    cout << "Give a string from which to search for: \n";
-    getline(cin, searchFrom);
-
-    cout << "Give search string: \n";
-    getline(cin, whatToSearch);
-
-    result = searchAString(searchFrom, whatToSearch);
-    if (result > 0)
+    try
     {
-        cout << "\""
-            << whatToSearch
-            << "\" found in \""
-            << searchFrom
-            << "\" in posiion "
-            << result;
-    }
-    else
-    {
-        cout << "\""
-            << whatToSearch
-            << "\" NOT found in \""
-            << searchFrom;
-    }
+        //We want to use this input method only 
+        // when program started witout arguments
+        if (argc == 1)
+        {
+            size_t result;
+            string searchFrom;
+
+            cout << "Give a string from which to search for: \n";
+            getline(cin, searchFrom);
+
+            cout << "Give search string: \n";
+            getline(cin, whatToSearch);
+
+            result = searchAString(searchFrom, whatToSearch);
+            if (result > 0)
+            {
+                cout << "\""
+                    << whatToSearch
+                    << "\" found in \""
+                    << searchFrom
+                    << "\" in posiion "
+                    << result;
+            }
+            else
+            {
+                cout << "\""
+                    << whatToSearch
+                    << "\" NOT found in \""
+                    << searchFrom;
+            }
+
+            return 0;
+        }
+
+        ifstream textFileToSearch(argv[2], std::ifstream::in);
+
+        result = searchAFile(textFileToSearch, argv[1]);
+
+        for (uint_fast8_t i = 0; i < result.size(); i++)
+        {
+            cout << result[i] << " \n";
+        }
+    } 	catch (exception e)
+	{
+		cout << "Error: " << e.what();
+	}
 }
 
